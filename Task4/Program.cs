18df3c7dd2c4a8084485fa4b8c20c597689d58b1
Denis <denis.tuapkin@gmail.com>
2024-01-03 21:45:24 +0500

    Max

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 9;
+int c = 100;
+int d = 23;
+int e = 15;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine (max);
